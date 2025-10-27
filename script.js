2ee15f9adc5a832a464f4a71b90ee051a7847da8
ThearyL <142918338+ThearyL@users.noreply.github.com>
2025-10-27 01:19:17 -0400

    avancement

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.from(".card-anim", {
         scrollTrigger: {
             trigger: ".card-anim",
+            start: "top 95%",
+            toggleActions: "play none none none",
+        },
+        y: 100,
+        opacity: 0,
+        duration: 0.8,
+        ease: "power2.out",
+        stagger: 0.2,
+
+    });
+
+
+    gsap.from(".contact", {
+        scrollTrigger: {
+            trigger: ".contact",
             start: "top 90%",
             toggleActions: "play none none none",
         },
@@ -11,21 +26,8 @@ document.addEventListener('DOMContentLoaded', () => {
         opacity: 0,
         duration: 0.8,
         ease: "power2.out",
-        stagger: 0.2
-    });
+        stagger: 0.2,
 
-
-    gsap.from(".contact", {
-        scrollTrigger: {
-            trigger: ".contact",
-            start: "top 96%",
-            toggleActions: "play none none none",
-        },
-        y: 100,
-        opacity: 0,
-        duration: 0.8,
-        ease: "power2.out",
-        stagger: 0.2
     });
 
 
@@ -34,17 +36,33 @@ document.addEventListener('DOMContentLoaded', () => {
 const app = Vue.createApp({
     data() {
         return {
-            theme: "dark"
+            theme: "dark",
+            donneesArr: [{
+                id: "",
+                title: "",
+                category: "",
+                images: []
+            }]
         };
     },
+    mounted() {
+        this.getProjet();
+    },
     methods: {
+        getProjet() {
+            fetch("./projets.json")
+                .then(data => data.json())
+                .then(infosArr => {
+                    this.donneesArr = infosArr;
+                    console.log(this.donneesArr);
+                })
+                .catch(() => {
+                    console.error("Erreur lors du chargement des données.");
+                });
+        },
         toggleTheme() {
             this.theme = this.theme === "dark" ? "light" : "dark";
             document.body.className = this.theme;
         }
     }
 }).mount("#app");
-
-
-
-
