527516ab586a1ef0172335502784ee8d443af1f4
ThearyL <142918338+ThearyL@users.noreply.github.com>
2025-10-29 11:25:51 -0400

    Version non-finale complete 1.0

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Animation GSAP avec scrolltrigger sur la page d'accueil
     gsap.registerPlugin(ScrollTrigger);
 
     gsap.from(".card-anim", {
@@ -32,56 +33,81 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 });
-
 const app = Vue.createApp({
     data() {
         return {
+            projet: null,
             theme: "dark",
             donneesArr: [{
                 id: "",
                 title: "",
+                shortDescription: "",
                 category: "",
+                skills: [],
                 images: []
             }]
         };
     },
     mounted() {
-        this.getProjet();
-       
-         const params = new URLSearchParams(window.location.search);
-         const projectId = params.get("proj-id");
- 
-         fetch("./projets.json")
-           .then(res => res.json())
-           .then(data => {
-             this.projet = data.find(p => p.id == projectId);
-           });
+        //Sauvegarde du theme
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            this.theme = savedTheme;
+            document.body.classList.add(savedTheme);
+        } else {
 
-           const match = document.cookie.match(/theme=(dark|light)/);
-           if (match) {
-             this.theme = match[1];
-             document.body.className = this.theme;
-           }
+            document.body.classList.add('dark');
+        }
+        //Pour obtenir et manipuler les paramètres de la chaîne de requête dans l'URL actuelle de la page.
+        const params = new URLSearchParams(window.location.search);
+        const projectId = params.get("proj-id");
+
+        //charge les données du fichier projets.json, trouve le projet correspondant à l’identifiant projectId, le stocke dans this.projet, puis exécute la fonction getProjet().
+        fetch("./projets.json")
+            .then(res => res.json())
+            .then(data => {
+
+                this.projet = data.find(p => p.id == projectId);
+            })
+            .catch(err => {
+                console.error("Erreur lors du chargement des données :", err);
+            });
+
+        this.getProjet();
+
+
+    }, computed: {
+        //Pour afficher la conception du projet avec le processus de creation
+        showProject() {
+            return this.projet && this.projet.id === 3;
+        }
     },
     methods: {
+        //Cherche les donnees dans le json
         getProjet() {
             fetch("./projets.json")
                 .then(data => data.json())
                 .then(infosArr => {
                     this.donneesArr = infosArr;
                     console.log(this.donneesArr);
+
                 })
                 .catch(() => {
                     console.error("Erreur lors du chargement des données.");
                 });
         },
+        //Changement du theme avec un comparaison et ajoute la classe au body
         toggleTheme() {
             this.theme = this.theme === "dark" ? "light" : "dark";
             document.body.className = this.theme;
-            document.cookie = `theme=${this.theme}; path=/; max-age=31536000`
+
+            localStorage.setItem('theme', this.theme);
+
         },
+        //Redirige vers la page projet.html en y ajoutant l’identifiant du projet (id) dans l’URL.
         goToProject(id) {
             window.location.href = `projet.html?proj-id=${id}`;
-          }
-    }
+        },
+
+    },
 }).mount("#app");
